a9a96ae4b8ca52aaa236d7d58768ab593ff03bbd
Alex Girard <me@alexgirard.ca>
2023-08-29 21:02:37 -0400

    v1 of dialog starting in normal mode

--- src/main/java/ca/alexgirard/harpoonIJ/HarpoonDialog.java
+++ src/main/java/ca/alexgirard/harpoonIJ/HarpoonDialog.java
@@ -1,10 +1,16 @@
 package ca.alexgirard.harpoonIJ;
 
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.EditorTextField;
+import com.maddyhome.idea.vim.command.OperatorArguments;
+import com.maddyhome.idea.vim.command.VimStateMachine;
+import com.maddyhome.idea.vim.helper.ModeHelper;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class HarpoonDialog extends DialogWrapper {
 
@@ -36,8 +42,26 @@ public class HarpoonDialog extends DialogWrapper {
             var settings = editor.getSettings();
             settings.setLineNumbersShown(true);
         });
+
+        editorTextField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (forcedNormalMode) return;
+                var editor = editorTextField.getEditor();
+                if (editor == null) return;
+                ModeHelper.exitInsertMode(editorTextField.getEditor(),
+                        DataManager.getInstance().getDataContext(e.getComponent()),
+                        new OperatorArguments(false, 1, VimStateMachine.Mode.COMMAND, VimStateMachine.SubMode.NONE));
+                editorTextField.setCaretPosition(0);
+                forcedNormalMode = true;
+            }
+
+            public void focusLost(FocusEvent e) {
+            }
+        });
         return editorTextField;
     }
+    private boolean forcedNormalMode = false;
+
     @Override
     protected JComponent createSouthPanel() {
         return null;
